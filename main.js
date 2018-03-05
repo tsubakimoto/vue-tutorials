033e5197a95cb60990a7f36645eae78d4e6b8e48
tsubakimoto <tkmbmbr@gmail.com>
2018-03-05 09:15:13 +0900

    complete tutorial#3

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const app = new Vue({
   el: "#app",
   data: {
-    bobby: {
-      first: "Bobby",
-      last: "Boone",
-      age: 25
-    },
-    john: {
-      first: "John",
-      last: "Boone",
-      age: 35
-    }
+    friends: [
+      {
+        first: "Bobby",
+        last: "Boone",
+        age: 25
+      },
+      {
+        first: "John",
+        last: "Boone",
+        age: 35
+      }
+    ],
   },
   compluted: {
     johnAgeInOneYear() {
@@ -22,15 +24,27 @@ const app = new Vue({
       return age + 1;
     },
     fullName(value) {
-      return `${value.last} ${value.first}`;
+      return `${value.last}, ${value.first}`;
     }
+  },
+  methods: {
+    decrementAge(friend) {
+      friend.age = friend.age - 1
+    },
+    incrementAge(friend) {
+      friend.age = friend.age + 1
+    },
   },
   template: `
     <div>
-      <h2>Name: {{john | fullName}}</h2>
-      <h2>Age: {{john.age | ageInOneYear}}</h2>
-      <h2>Name: {{bobby | fullName}}</h2>
-      <h2>Age: {{bobby.age | ageInOneYear}}</h2>
+      <h2 v-for="friend in friends">
+        <h4>{{friend | fullName}}</h4>
+        <h5>age: {{friend.age}}</h5>
+        <button v-on:click="decrementAge(friend)">-</button>
+        <button v-on:click="incrementAge(friend)">+</button>
+        <input v-model="friend.first"/>
+        <input v-model="friend.last"/>
+      </h2>
     </div>
   `
 });
